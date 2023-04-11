905fb6f6b3ff356d77e7b62e832e1b67ce9ea255
Guillermo <guilledev98@gmail.com>
2023-04-11 20:46:30 +0200

    Adhesión del menú desplegable con la información del carrito de compras al icono del carrito del header y aplicación de la performance con JS

--- js/app.js
+++ js/app.js
@@ -1,21 +1,38 @@
-// Variables
+// VARIABLES
+
+//Desplegables
 const linkMenuMobile = document.querySelector('#link-menu--mobile');
-const linkMenuDesktop = document.querySelector('#link-menu--desktop');
-const menuDesktop = document.querySelector('#menu--desktop');
 const menuMobile = document.querySelector('#menu--mobile');
 
+const linkMenuDesktop = document.querySelector('#link-menu--desktop');
+const menuDesktop = document.querySelector('#menu--desktop');
 
-//addEventListeners
+const linkMenuCarrito = document.querySelector('#link-menu--carrito');
+const menuCarrito = document.querySelector('#menu--carrito');
+
+console.log();
+
+
+//ADDEVENTLISTENERS
 document.addEventListener('DOMContentLoaded', () => {
-    linkMenuMobile.addEventListener('click', toggleMobileMenu);
-    linkMenuDesktop.addEventListener('click', toggleDesktopMenu);
+
+    // Desplegables
+    linkMenuMobile.addEventListener('click', e => {
+        toggleMenu(menuMobile);
+    });
+    linkMenuDesktop.addEventListener('click', e => {
+        toggleMenu(menuDesktop);
+    });
+    linkMenuCarrito.addEventListener('click', e => {
+        toggleMenu(menuCarrito);
+    });
+
 });
 
 
-//Funciones 
-function toggleMobileMenu() {
-    menuMobile.classList.toggle('inactive');
-}
-function toggleDesktopMenu() {
-    menuDesktop.classList.toggle('inactive');
+//FUNCIONES
+
+// Desplegables
+function toggleMenu(menu) {
+    menu.classList.toggle('inactive');
 };
