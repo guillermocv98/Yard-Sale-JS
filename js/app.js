c468a21c1d39c9b82b566c5187b83e8c6c75b073
Guillermo <guilledev98@gmail.com>
2023-04-11 19:51:11 +0200

    Fusión del header principal con el menu mobile y aplicación de la performance con JS

--- js/app.js
+++ js/app.js
@@ -2,15 +2,20 @@
 const linkMenuMobile = document.querySelector('#link-menu--mobile');
 const linkMenuDesktop = document.querySelector('#link-menu--desktop');
 const menuDesktop = document.querySelector('#menu--desktop');
+const menuMobile = document.querySelector('#menu--mobile');
 
 
 //addEventListeners
 document.addEventListener('DOMContentLoaded', () => {
+    linkMenuMobile.addEventListener('click', toggleMobileMenu);
     linkMenuDesktop.addEventListener('click', toggleDesktopMenu);
 });
 
 
 //Funciones 
+function toggleMobileMenu() {
+    menuMobile.classList.toggle('inactive');
+}
 function toggleDesktopMenu() {
     menuDesktop.classList.toggle('inactive');
 };
